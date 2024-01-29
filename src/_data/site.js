d378fd3d09b70d7734936478d09baa24926b5987
arpit-agr <arpit.agrawal2502@gmail.com>
2024-01-29 12:15:26 +0530

    update site global data

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,26 +1,15 @@
 module.exports = {
   url: process.env.URL || 'http://localhost:8080',
   siteName: 'Ambco Hygiene Care',
-  siteDescription: 'Professional & Innovative Cleaning Tools & Accessories',
-  siteType: 'Person', // schema
+  siteShortName: 'Ambco',
+  siteDescription:
+    'Ambco Hygiene Care is your trusted partner for professional and innovative cleaning tools and accessories.',
   locale: 'en',
   lang: 'en',
-  author: '', // i.e. author's name. Must be set.
-  authorEmail: '', // i.e. email of the author
-  authorWebsite: '', // i.e. the personal site of the author
   meta_data: {
     opengraph_default: '/assets/images/opengraph-default.jpg', // fallback/default meta image
     opengraph_default_alt:
-      'Visible content: Eleventy starter based on workflow for Cube CSS, Every Layout, Design Tokens and Tailwind for uitility, based on the concepts explained in buildexcellentwebsit.es ', // alt text for default meta image
-    twitterSite: '', // i.e. @site - twitter profile of the site
-    twitterCreator: '', // i.e. @author -  twitter profile of the author
-    mastodonProfile: '' // i.e. url to the author's mastodon instance/profile
-  },
-  blog: {
-    // this is for the rss feed
-    name: 'My great Web Development Blog',
-    description:
-      'Tell the word what you are writing about in your blog! It will show up on feed readers.'
+      'Visible content: Eleventy starter based on workflow for Cube CSS, Every Layout, Design Tokens and Tailwind for uitility, based on the concepts explained in buildexcellentwebsit.es ' // alt text for default meta image
   },
   address: {
     firm: 'Ambco Hygiene Care',
